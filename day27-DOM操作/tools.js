710444f5e1be0c4b1676f6c29c966c752e0e4850
caiyun <sanbei712@163.com>
2021-12-17 09:51:41 +0800

    2021-12-18

---
+++ day27-DOM操作/tools.js
@@ -0,0 +1,10 @@
+/* 专门存储函数方法的js外部文件 */
+// 使用兼容语法获取标签css样式的属性值
+function myGetStyle (element,type) {
+    /* if (window.getComputedStyle) {
+        return window.getComputedStyle(element)[type]
+    } else {
+        return element.currentStyle[type]
+    } */
+    
+}
---
+++ day28-DOM操作/tool.js
@@ -0,0 +1,8 @@
+/* 使用兼容语法获取css样式的方法 */
+function myGetStyle(element,type) {
+    if (window.getComputedStyle) {
+        return window.getComputedStyle(element)[type] 
+    } else {
+        return element.currentStyle[type]
+    }
+}
